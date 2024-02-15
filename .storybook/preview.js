d7c120464f83a8e867f25fc3816969ba54e914ca
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2024-02-15 09:49:14 -0600

    feat: correct install paths for twig namespaces

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,9 @@
-import { addDecorator } from '@storybook/html';
 import { useEffect } from '@storybook/client-api';
 import Twig from 'twig';
 import { setupTwig } from './setupTwig';
 
 // GLOBAL CSS
-import '../components/style.scss';
+import '../../../components/style.scss';
 
 // If in a Drupal project, it's recommended to import a symlinked version of drupal.js.
 import './_drupal.js';
--- .storybook/setupTwig.js
+++ .storybook/setupTwig.js
@@ -11,28 +11,28 @@ const twigAddAttributes = require('add-attributes-twig-extension');
  */
 const fetchVariantConfig = () => {
   try {
-    return require('../project.emulsify.json').variant.structureImplementations;
+    return require('../../../project.emulsify.json').variant.structureImplementations;
   } catch (e) {
     return [
       {
         name: 'base',
-        directory: 'components/00-base',
+        directory: '../../../components/00-base',
       },
       {
         name: 'atoms',
-        directory: 'components/01-atoms',
+        directory: '../../../components/01-atoms',
       },
       {
         name: 'molecules',
-        directory: 'components/02-molecules',
+        directory: '../../../components/02-molecules',
       },
       {
         name: 'organisms',
-        directory: 'components/03-organisms',
+        directory: '../../../components/03-organisms',
       },
       {
         name: 'templates',
-        directory: 'components/04-templates',
+        directory: '../../../components/04-templates',
       },
     ];
   }
