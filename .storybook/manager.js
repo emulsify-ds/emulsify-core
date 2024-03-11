d2a06c04b3a2c6ea9f7b5d6ffe93666157620a66
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2024-03-11 09:46:56 -0500

    chore: remove manager importing

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,18 +2,6 @@ import { addons } from '@storybook/addons';
 
 import emulsifyTheme from './emulsifyTheme';
 
-// Include custom theme preview config if it exists.
-(async () => {
-  let theme;
-  try {
-    theme = await import('../../../config/storybook/theme');
-  } catch (e) {
-    addons.setConfig({
-      theme: emulsifyTheme,
-    });
-  } finally {
-    addons.setConfig({
-      theme: theme,
-    });
-  }
-})()
+addons.setConfig({
+  theme: emulsifyTheme,
+});
