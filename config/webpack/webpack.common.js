9bf2e6da1ffe90c107ba2723a2f6649af79bf962
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2024-08-30 12:17:00 -0500

    feat: allow older components structure to compile

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -17,19 +17,20 @@ const srcDir = fs.existsSync(path.resolve(projectDir, 'src'))
 // Glob pattern for scss files that ignore file names prefixed with underscore.
 const BaseScssPattern = path.resolve(
   srcDir,
-  '{tokens,foundation,layout}/**/!(_*|cl-*|sb-*).scss',
+  '!(components|util)/**/!(_*|cl-*|sb-*).scss',
 );
-const ComponentScssPattern = path.resolve(
-  srcDir,
-  'components/**/!(_*|cl-*|sb-*).scss',
-);
+const ComponentScssPattern = fs.existsSync(path.resolve(projectDir, 'src'))
+  ? path.resolve(srcDir, 'components/**/!(_*|cl-*|sb-*).scss')
+  : path.resolve(srcDir, '**/!(_*|cl-*|sb-*).scss');
 const ComponentLibraryScssPattern = path.resolve(srcDir, 'util/**/!(_).scss');
 
 // Glob pattern for JS files.
-const jsPattern = path.resolve(
-  srcDir,
-  'components/**/!(*.stories|*.component|*.min|*.test).js',
-);
+const jsPattern = fs.existsSync(path.resolve(projectDir, 'src'))
+  ? path.resolve(
+      srcDir,
+      'components/**/!(*.stories|*.component|*.min|*.test).js',
+    )
+  : path.resolve(srcDir, '**/!(*.stories|*.component|*.min|*.test).js');
 
 // Glob pattern for svgSprite config.
 const spritePattern = path.resolve(webpackDir, 'svgSprite.js');
@@ -44,13 +45,15 @@ function getEntries(
 ) {
   const entries = {};
 
-  // Token/Foundation/Layout SCSS entries.
+  // Non-component or global SCSS entries.
   glob.sync(BaseScssMatcher).forEach((file) => {
-    const filePath = file.split(/(tokens\/|foundation\/|layout\/)/)[2];
+    const filePath = file.split(`${srcDir}/`)[1];
     const filePathDist = filePath.split('/')[1]
       ? filePath.split('/')[1]
       : filePath.split('/')[0];
-    const newfilePath = `dist/global/${filePathDist.replace('.scss', '')}`;
+    const newfilePath = fs.existsSync(path.resolve(projectDir, 'src'))
+      ? `dist/global/${filePathDist.replace('.scss', '')}`
+      : `dist/css/${filePathDist.replace('.scss', '')}`;
     entries[newfilePath] = file;
   });
 
@@ -58,10 +61,14 @@ function getEntries(
   glob.sync(ComponentScssMatcher).forEach((file) => {
     const filePath = file.split('components/')[1];
     const filePathDist = filePath.replace('/', '/css/');
+    const distStructure = fs.existsSync(path.resolve(projectDir, 'src'))
+      ? 'components'
+      : 'css';
     const newfilePath =
-      emulsifyConfig.project.platform === 'drupal'
+      emulsifyConfig.project.platform === 'drupal' &&
+      fs.existsSync(path.resolve(projectDir, 'src'))
         ? `components/${filePathDist.replace('.scss', '')}`
-        : `dist/components/${filePathDist.replace('.scss', '')}`;
+        : `dist/${distStructure}/${filePathDist.replace('.scss', '')}`;
     entries[newfilePath] = file;
   });
 
@@ -77,10 +84,13 @@ function getEntries(
     if (!file.includes('dist/')) {
       const filePath = file.split('components/')[1];
       const filePathDist = filePath.replace('/', '/js/');
+      const distStructure = fs.existsSync(path.resolve(projectDir, 'src'))
+        ? 'components'
+        : 'js';
       const newfilePath =
         emulsifyConfig.project.platform === 'drupal'
           ? `components/${filePathDist.replace('.js', '')}`
-          : `dist/components/${filePathDist.replace('.js', '')}`;
+          : `dist/${distStructure}/${filePathDist.replace('.js', '')}`;
       entries[newfilePath] = file;
     }
   });
