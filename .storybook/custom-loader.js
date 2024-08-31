1c6f1b9b79c2b4b978ce37f175fd8a51e9ef1835
Roberto Hernandez <roberthb94@gmail.com>
2024-08-31 00:14:45 -0600

    feat: add a custom loader for twig files

---
+++ .storybook/custom-loader.js
@@ -0,0 +1,5 @@
+module.exports = function (source) {
+  const projectName = this.getOptions().projectName || '';
+  const result = source.replace(new RegExp(`${projectName}:`, 'g'), `${projectName}/`);
+  return result;
+};
