91f239b50471b72aea939d1e0c77eafcd39bb6fe
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2025-05-19 10:23:14 -0500

    fix: bypass the plugin’s internal “require by string” logic by importing each plugin

--- release.config.mjs
+++ release.config.mjs
@@ -1,18 +1,12 @@
 // release.config.mjs
-import commitAnalyzer from '@semantic-release/commit-analyzer';
-import releaseNotes from '@semantic-release/release-notes-generator';
-import npm from '@semantic-release/npm';
-import github from '@semantic-release/github';
-
 export default {
   tagFormat: '${version}',
   branches: ['main'],
   repositoryUrl: 'git@github.com:emulsify-ds/emulsify-core.git',
   plugins: [
-    // explicit function + (optional) config object
-    [commitAnalyzer, { preset: 'angular' }],
-    [releaseNotes],
-    [npm, { npmPublish: false }],
-    [github],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/npm', { npmPublish: false }],
+    '@semantic-release/github',
   ],
 };
