d507c9007bc97a51cda7106189c5457bc7049c10
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2025-05-26 16:44:22 -0500

    fix: workaround for parserOpts not a function error during release

--- release.config.cjs
+++ release.config.cjs
@@ -3,18 +3,28 @@ module.exports = {
   branches: ['main'],
   repositoryUrl: 'git@github.com:emulsify-ds/emulsify-core.git',
   plugins: [
-    ['@semantic-release/commit-analyzer', {
-      preset: 'angular',
-      releaseRules: [
-        { type: 'feat', release: 'minor' },
-        { type: 'fix',  release: 'patch' }
-      ],
-      parserOpts: {
-        noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+        }
       }
-    }],
-    '@semantic-release/release-notes-generator',
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+        },
+        writerOpts: {
+          commitsSort: ['subject', 'scope']
+        }
+      }
+    ],
     ['@semantic-release/npm', { npmPublish: false }],
     '@semantic-release/github'
   ]
-};
+}
